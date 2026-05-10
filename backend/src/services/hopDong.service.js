d8e4b57504e0c69049743356a2a94636dc57ce71
danwanh <dananh890@gmail.com>
2026-05-10 12:37:47 +0700

    fix: move methods to hopdong services

--- backend/src/services/hopDong.service.js
+++ backend/src/services/hopDong.service.js
@@ -1,4 +1,6 @@
 const hopDongDAO = require('../dao/hopDong.dao');
+const dichVuBUS = require('./dichVu.service');
+
 
 class HopDong_BUS {
   static async LayTheoMa(maHD) {
@@ -16,6 +18,34 @@ class HopDong_BUS {
   static async CapNhatTrangThai(ma, tt) {
     return await hopDongDAO.capNhatTrangThai(ma, tt);
   }
+
+  static TinhTongGiaGiuong(hopDong) {
+    const tienThueThang = Number(hopDong?.giathue) || 0;
+    return tienThueThang;
+  }
+
+  static async LayThongTinThanhToanKyDau(maHD) {
+    // 1. Lấy thông tin hợp đồng
+    const hopDong = await this.LayTheoMa(maHD);
+    if (!hopDong) throw new Error('Không tìm thấy hợp đồng');
+
+    // 2. Tính tổng giá giường = tiền thuê hợp đồng
+    const tongGiaGiuong = this.TinhTongGiaGiuong(hopDong);
+
+    // 3. Lấy thông tin chi nhánh để lấy dịch vụ
+    const maCN = hopDong.hop_dong_giuong?.[0]?.giuong?.phong?.macn;
+    let dichVu = [];
+    if (maCN) {
+      dichVu = await dichVuBUS.LayTheoMaCN(maCN);
+    }
+
+    return {
+      hopDong,
+      tongGiaGiuong,
+      dichVu
+    };
+  }
+
 }
 
 module.exports = HopDong_BUS;
